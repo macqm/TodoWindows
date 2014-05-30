c593de655ca3fcd576460f6bbcb6a3b5987fb0df
Maciej Kumorek <mkumorek@gmail.com>
2014-05-30 01:17:04 +0100

    Removed some legacy Windows 8 code

--- ParseTodo/ParseTodo/MainPage.xaml.cs
+++ ParseTodo/ParseTodo/MainPage.xaml.cs
@@ -22,29 +22,43 @@ namespace ParseTodo
     /// <summary>
     /// A basic page that provides characteristics common to most applications.
     /// </summary>
-    public sealed partial class MainPage : ParseTodo.Common.LayoutAwarePage
+    public sealed partial class MainPage : Page
     {
+        /// <summary>
+        /// Default view model
+        /// </summary>
+        private readonly Dictionary<string, object> defaultViewModel = new Dictionary<string, object>();
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
         /// <summary>
+        /// Gets Default View Model
+        /// </summary>
+        internal Dictionary<string, object> DefaultViewModel
+        {
+            get
+            {
+                return this.defaultViewModel;
+            }
+        }
+
+        /// <summary>
         /// Populates the page with content passed during navigation.  Any saved state is also
         /// provided when recreating a page from a prior session.
         /// </summary>
-        /// <param name="navigationParameter">The parameter value passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
-        /// </param>
-        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
-        /// session.  This will be null the first time a page is visited.</param>
-        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        /// <param name="e">Navigation event arguments</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             DefaultViewModel["SelectedTodo"] = null;
             DefaultViewModel["IsEditing"] = false;
             DefaultViewModel["CanEdit"] = false;
             DefaultViewModel["IsSaving"] = false;
             Refresh();
+
+            base.OnNavigatedTo(e);
         }
 
         private async void Refresh()
@@ -79,16 +93,11 @@ namespace ParseTodo
             await dialog.ShowAsync();
         }
 
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
-        protected override void SaveState(Dictionary<String, Object> pageState)
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
+ 	         base.OnNavigatedFrom(e);
         }
-
+        
         private void AddTodoItem(object sender, RoutedEventArgs e)
         {
             var newItem = new TodoItem();
